726c88bda03ef88ba8a102e23d350d177f0e09b8
Altyn <anhkhoa3030@gmail.com>
2024-11-12 01:56:20 +0700

    lab 3.1

--- DaoAnhKhoa-ps44222-lab3.c
+++ DaoAnhKhoa-ps44222-lab3.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
 int main(void)
 {
     printf("1. Chuong trinh tinh hoc luc\n ");
@@ -48,35 +49,89 @@ int main(void)
         }
     }
     break;
-    case 2:{
+    case 2:
+    {
         printf("Chuong trinh giai phuong trinh bac 1 :");
-    float a, b, x;
-    printf("Nhap he so a: ");
-    scanf("%f", &a);
-    printf("Nhap he so b: ");
-    scanf("%f", &b);
-    if (a == 0) {
-        if (b == 0) {
-            printf("Phuong trinh vo so nghiem.\n");
-        } else {
-            printf("Phuong trinh vo nghiem.\n");
+        float a, b, x;
+        printf("Nhap he so a: ");
+        scanf("%f", &a);
+        printf("Nhap he so b: ");
+        scanf("%f", &b);
+        if (a == 0)
+        {
+            if (b == 0)
+            {
+                printf("Phuong trinh vo so nghiem.\n");
+            }
+            else
+            {
+                printf("Phuong trinh vo nghiem.\n");
+            }
         }
-    } else {
-        // Tính nghiệm x
-        x = -b / a;
-        printf("Nghiem cua phuong trinh la: x = %.2f\n", x);
-    } 
+        else
+        {
+            // Tính nghiệm x
+            x = -b / a;
+            printf("Nghiem cua phuong trinh la: x = %.2f\n", x);
+        }
     }
-        break;
-    case 3:{
+    break;
+    case 3:
+    {
         printf("Chuong trinh giai phuong trinh bac 2");
-    }   
-        break;
-    case 4:{
-        printf("Dap an ban chon da sai");
+        float a, b, c, x, delta;
+
+        x = 0;
+        printf("Vui long nhap he so a,b,c");
+        scanf("%f%f%f", &a, &b, &c);
+        if (a == 0)
+        {
+            if (b == 0)
+            {
+                if (c == 0)
+                {
+                    printf("Phuong trinh co vo so nghiem. \n");
+                }
+                else
+                {
+                    printf("Phuong trinh Vo nghiem . \n");
+                }
+            }
+            else
+            {
+                x = -c / b;
+                printf("Phuong trinh co mot nghiem: x = %.2f \n", x);
+            }
+        }
+        else
+        {
+            delta = b * b - 4 * a * c;
+            if (delta < 0)
+            {
+                printf("Phuong trinh vo nghiem. \n");
+            }
+            else if (delta == 0)
+            {
+                x = -b / (2 * a);
+                printf("Phuong trinh co nghiem kep: x=%.2f \n", x);
+            }
+            else
+            {
+                double x1 = (-b + sqrt(delta)) / (2 * a);
+
+                double x2 = (-b - sqrt(delta)) / (2 * a);
+                printf("Phuong trinh co hai nghiem phan biet: x1 = %.2lf va x2 =%.2lf\n", x1, x2);
+            }
+        }
     }
+    break;
+    case 4:
+    {
+        printf("Chuong trinh xay dung tinh tien dien");
         
-        break;
+    }
+
+    break;
     case 0:
         printf("See You Again");
         exit(0);
